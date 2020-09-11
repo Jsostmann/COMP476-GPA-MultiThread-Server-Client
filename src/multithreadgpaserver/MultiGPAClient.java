e1198c04ca22675233655f8570dd3a4a187e768c
James Ostmann <jsostmann@aggies.ncat.edu>
2020-09-11 15:58:15 -0400

    First Commit

---
+++ src/multithreadgpaserver/MultiGPAClient.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package multithreadgpaserver;
+
+
+import java.io.BufferedInputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class MultiGPAClient {
+
+/**
+ *
+ * @author jamesostmann
+ */
+    private DataInputStream fromServer = null;
+    private DataOutputStream toServer = null;
+    private Socket socket = null;
+   
+    public static void main(String[] args) {
+        new MultiGPAClient(9989,new String[] {"4, A-, 3, A, 3, C+, 4, B, 3, 2.88, 46",
+                                               "5, A, 3, B+, 4, A, 1, C, 3, A-, 3, 3.12, 75"});
+    }
+    public MultiGPAClient(int port,String[] messages) {
+        try {
+            socket = new Socket(InetAddress.getLocalHost(),port);
+            System.out.println(InetAddress.getLocalHost().getHostAddress());
+            fromServer = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
+            toServer = new DataOutputStream(socket.getOutputStream());
+            
+            for(int i = 0; i < messages.length; i++) {
+                
+                System.out.println("Sending data to server...");
+                toServer.writeUTF(messages[i]);
+                
+                String answer = fromServer.readUTF();
+                String[] values = answer.split(", ");
+                System.out.println("Semester GPA : " + values[0]);
+                System.out.println("Cumulative GPA: " + values[1]);
+                System.out.println("Total Credits: " + values[2]);
+                
+            }
+            System.out.println("Sending stop to server...");
+            toServer.writeUTF("Ok");            
+            
+        } catch (UnknownHostException ex) {
+            Logger.getLogger(MultiGPAClient.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(MultiGPAClient.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+   
+}
---
+++ src/multithreadgpaserver/MultiGPAServer.java
@@ -0,0 +1,159 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package multithreadgpaserver;
+
+import java.io.BufferedInputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.text.DecimalFormat;
+import java.util.HashMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author jamesostmann
+ */
+public class MultiGPAServer {
+    
+    private ServerSocket server;
+    private static final HashMap<String, Double> GPA_TABLE;
+    
+    public static void main(String[] args) {
+        new MultiGPAServer(9989);
+    }
+    
+    static {
+        GPA_TABLE = new HashMap<>();
+        initTable();
+    }
+
+    public MultiGPAServer(int port) {
+
+        int cores = Runtime.getRuntime().availableProcessors();
+        System.out.println("Starting server with " + cores + " cores");
+
+        try {
+            server = new ServerSocket(port);
+
+            while (true) {
+
+                Socket socket = server.accept();
+                InetAddress ip = socket.getLocalAddress();
+                
+                System.out.println("Established connection with new client at " + ip.getHostAddress());
+                
+                new GPAThread(socket).start();
+
+            }
+
+        } catch (IOException ex) {
+            Logger.getLogger(MultiGPAServer.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
+    private static String calculateGPA(String clientMessage) {
+
+        String[] message = clientMessage.split(", ");
+        int numGrades = Character.getNumericValue(message[0].charAt(0));
+        int i;
+        int semesterCredits = 0;
+        double semesterTotal = 0;
+
+        for (i = 1; i < numGrades * 2; i += 2) {
+            semesterCredits += Integer.parseInt(message[i + 1]);
+            semesterTotal += (GPA_TABLE.get(message[i]) * Integer.parseInt(message[i + 1]));
+        }
+
+        DecimalFormat df = new DecimalFormat("#.##");
+        String semesterGPA = df.format(semesterTotal / semesterCredits);
+
+        double prevGPA = Double.parseDouble(message[i++]);
+        int prevCreditHrs = Integer.parseInt(message[i]);
+        double cumulativeGPANum = ((prevGPA * prevCreditHrs) + semesterTotal) / (prevCreditHrs += semesterCredits);
+        String cumulativeGPA = df.format(cumulativeGPANum);
+
+        return semesterGPA + ", " + cumulativeGPA + ", " + String.valueOf(prevCreditHrs);
+
+        /*
+        int real = 0;
+        
+        for(i = 0; i < numGrades; i++) {
+            System.out.println(grades[real + 1] + " " + grades[real+2]);
+            real+=2;
+        }
+         */
+    }
+
+    static void initTable() {
+        GPA_TABLE.put("A", 4.0);
+        GPA_TABLE.put("A-", 3.7);
+        GPA_TABLE.put("B+", 3.3);
+        GPA_TABLE.put("B", 3.0);
+        GPA_TABLE.put("B-", 2.7);
+        GPA_TABLE.put("C+", 2.3);
+        GPA_TABLE.put("C", 2.0);
+        GPA_TABLE.put("C-", 1.7);
+        GPA_TABLE.put("D+", 1.3);
+        GPA_TABLE.put("D", 1.0);
+        GPA_TABLE.put("F", 0.0);
+    }
+
+    static boolean notFinished(String clientMessage) {
+        return !clientMessage.trim().equals("Ok");
+    }
+
+    public class GPAThread extends Thread {
+
+        private Socket socket;
+        private DataOutputStream toClient;
+        private DataInputStream fromClient;
+
+        public GPAThread(Socket socket) {
+            this.socket = socket;
+        }
+
+        @Override
+        public void run() {
+
+            try {
+                
+                fromClient = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
+
+                toClient = new DataOutputStream(socket.getOutputStream());
+
+                String clientMessage = fromClient.readUTF();
+
+                System.out.println("Recieved message from client:  " + clientMessage);
+
+                while (notFinished(clientMessage)) {
+
+                    String reply = calculateGPA(clientMessage);
+                    toClient.writeUTF(reply);
+                    
+                    clientMessage = fromClient.readUTF();
+                    System.out.println("Recieved message from client:  " + clientMessage);
+                }
+                
+            socket.close();
+            toClient.close();
+            fromClient.close();
+            
+            System.out.println("Socket and thread closed");
+                
+            } catch (IOException ex) {
+                Logger.getLogger(MultiGPAServer.class.getName()).log(Level.SEVERE, null, ex);
+            }
+
+        }
+
+    }
+}
